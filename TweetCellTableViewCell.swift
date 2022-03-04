0ca740caa7f136b9e9ea6f1c00df344204145fe4
Tatum Allen <82176414+tatumalllen@users.noreply.github.com>
2022-03-03 19:19:14 -0500

    Add files via upload

---
+++ TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Tatum Allen on 3/2/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var tweetImageView: UILabel!
+
+    @IBOutlet weak var UsernameImageView: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
