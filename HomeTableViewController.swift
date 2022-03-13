e6e8c2e8bb0a1f1e470615f3d15e839bbb50f274
Tatum Allen <82176414+tatumalllen@users.noreply.github.com>
2022-03-12 21:16:18 -0500

    Update HomeTableViewController.swift

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -21,6 +21,12 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
     }
     
     @objc func loadTweet(){
@@ -79,8 +85,8 @@ class HomeTableViewController: UITableViewController {
     }
     
     
-    @IBAction func onLogout(_ sender: Any)
-    {
+    
+    @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
@@ -101,6 +107,11 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
